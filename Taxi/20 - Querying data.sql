6e2d3dc4a10b85d964bee023ee5269f08c75d0a2
Wesley Backelant <wesback@users.noreply.github.com>
2018-05-14 11:14:47 +0200

    Update: LICENSE README.md Taxi

--- Taxi/20 - Querying data.sql
+++ Taxi/20 - Querying data.sql
@@ -1,7 +1,45 @@
 -- Databricks notebook source
-SELECT * FROM taxi_tripdata_yellow LIMIT 20
+DESCRIBE taxi_tripdata_yellow
 
 -- COMMAND ----------
 
-SELECT vendor_name, SUM(Passenger_Count) AS TotalPassengers
-FROM taxi_tripdata_yellow GROUP BY vendor_name
+SELECT PaymentType, COUNT(*) 
+FROM taxi_tripdata_yellow 
+GROUP BY PaymentType
+
+-- COMMAND ----------
+
+SELECT Year, Month, COUNT(*) 
+FROM taxi_tripdata_yellow 
+GROUP BY Year, Month
+ORDER BY Year, Month
+
+-- COMMAND ----------
+
+SELECT PaymentType, COUNT(*) FROM taxi_tripdata_yellow GROUP BY PaymentType
+
+-- COMMAND ----------
+
+select * from (
+select 
+ROUND(PickupLongitude, 4), 
+ROUND(PickupLatitude, 4),
+count(*) as c
+from taxi_tripdata_yellow 
+WHERE 
+PickupLongitude IS NOT NULL
+AND
+PickupLatitude IS NOT NULL
+AND
+year = 2014
+AND
+month = 2
+AND
+ROUND(PickupLongitude, 4) BETWEEN -74.259090 AND -73.9
+AND
+ROUND(PickupLatitude, 4) BETWEEN 40.477399 AND 40.917577
+GROUP BY 
+ROUND(PickupLongitude, 4), 
+ROUND(PickupLatitude, 4)
+  ) 
+  WHERE c > 5
